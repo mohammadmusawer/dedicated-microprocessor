c5b670a9205e989b6cfdba33201eeddb19072ca9
Mohammad Musawer <66740014+mohammadmusawer@users.noreply.github.com>
2021-07-03 10:35:09 -0400

    Created a 2-to-1 multiplexor module

--- microprocessor.v
+++ microprocessor.v
@@ -10,5 +10,23 @@ module or_gate(
 
 endmodule
 
+//2-to-1 mux module
+module mux(
+    input [7:0]a, b,
+    input op,
+    output reg [7:0]out
+    );
+
+    always @(*)
+    begin
+        case (op)
+        1'b1: out = a;  //if operation is 1, let in a
+        1'b0: out = b;  //if operation is 0, let in b
+        default: out = 1'bx;
+        endcase
+    end
+
+endmodule
+
 module control_unit();
 endmodule
